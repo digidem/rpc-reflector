c349ff74e6d020846b8c99d198ed26cf280de3b6
Gregor MacLennan <gmaclennan@digital-democracy.org>
2025-07-09 18:12:17 +0100

    feat: add logger option
    Supports passing a logger that implements the pino interface. For improved debug of rpc message errors.

--- test/validate-message.test.js
+++ test/validate-message.test.js
@@ -1,19 +1,37 @@
 import test from 'tape'
-import { isValidMessage } from '../lib/validate-message.js'
-import validMessages from './fixtures/valid-messages.js'
+import {
+  validateRequestMsg,
+  validateResponseMsg,
+} from '../lib/validate-message.js'
+import {
+  validRequestMessages,
+  validResponseMessages,
+} from './fixtures/valid-messages.js'
 import invalidMessages from './fixtures/invalid-messages.js'
 
 test('Valid messages return true', (t) => {
-  for (const msg of validMessages) {
-    t.ok(isValidMessage(msg), `Message \`${JSON.stringify(msg)}\` is valid`)
+  for (const msg of validRequestMessages) {
+    t.doesNotThrow(
+      () => validateRequestMsg(msg),
+      `Message \`${JSON.stringify(msg)}\` is valid`,
+    )
+  }
+  for (const msg of validResponseMessages) {
+    t.doesNotThrow(
+      () => validateResponseMsg(msg),
+      `Message \`${JSON.stringify(msg)}\` is valid`,
+    )
   }
   t.end()
 })
 
 test('Invalid mesages return false', (t) => {
   for (const msg of invalidMessages) {
-    t.false(
-      isValidMessage(msg),
+    t.throws(
+      () => {
+        validateRequestMsg(msg)
+        validateResponseMsg(msg)
+      },
       `Message \`${JSON.stringify(msg)}\` is invalid`,
     )
   }
